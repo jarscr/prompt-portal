4cc4d784088dab0b2deb7ea6889c97a6cced7947
Alfredo <jarscr@gmail.com>
2026-03-26 18:58:15 -0600

    Ads 1

--- src/components/AdPlacement.jsx
+++ src/components/AdPlacement.jsx
@@ -1,7 +1,8 @@
 export default function AdPlacement({ id, className = '' }) {
   return (
     <div id={id} className={`promo-slot ${className}`}>
-      <p>Espacio publicitario</p>
+      <p><a href="https://www.tkqlhce.com/click-100501996-14573809" target="_top">
+      <img src="https://www.tqlkg.com/image-100501996-14573809" width="250" height="360" alt="" border="0"/></a></p>
     </div>
   );
 }
